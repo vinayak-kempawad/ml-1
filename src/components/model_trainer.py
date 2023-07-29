24a970689174c04017670b0c12e5819545a10c7b
vinayak-kempawad <vinayak.kempawad@gmail.com>
2023-07-29 20:09:34 +0530

    Model Trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -21,7 +21,7 @@ from src.utils import save_object, evaluate_model
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join('atrifacts', "model.pkl")
+    trained_model_file_path=os.path.join('artifacts', "model.pkl")
     
 class ModelTrainer:
     def __init__(self):
